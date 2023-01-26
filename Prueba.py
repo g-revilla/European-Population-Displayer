f1b0f0b21905434839922eb6733557483316cb97
Gonzalo Revilla <gonzaloremu@gmail.com>
2023-01-26 21:43:46 +0100

    Commit y push inicial del proyecto :)

---
+++ Prueba.py
@@ -0,0 +1,10 @@
+# este es un scrip de prueba, debes de ser capaz de abrirlo y ejecutarlo
+# además tienes que tener Git enlazado en Pycharm, por tanto deberás ser capaz de hacer commit, pushes
+# desde el mismo script
+
+nombre = 'Daniel'
+
+print(f"\n\nBuenos dias querido {nombre}, bienvenido a este tutorial de Pycharm junto con Git, espero que estes listo para empezar la start-up y forrarnos.\n\n")
+
+print("Let's fucking gooo!!!!\n\n")
+# ya te enseñaré a hacer esto cuando nos veamos
